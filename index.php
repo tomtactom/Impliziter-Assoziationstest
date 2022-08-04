22db9ea8077c26c64a59e3a8db4fa34d92d2d572
Tom <32821177+tomtactom@users.noreply.github.com>
2022-08-04 11:06:47 +0200

    Update index.php

--- index.php
+++ index.php
@@ -3,9 +3,9 @@
 		<title>IAT</title>
 		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 		<!--<link type="text/css" href="core/css/overcast/jquery-ui-1.8.18.custom.css" rel="stylesheet" />-->
-		<!--<style type="text/css"> @import "core/css/iat.css";</style>-->
+		<style type="text/css"> @import "core/css/iat.css";</style>
 		<script type="text/javascript" src="core/js/jquery-1.7.1.min.js"></script>
-		<script type="text/javascript" src="core/js/jquery-ui-1.8.18.custom.min.js"></script>
+		<!--<script type="text/javascript" src="core/js/jquery-ui-1.8.18.custom.min.js"></script>-->
 		<script type="text/javascript" src="core/js/IAT.js"></script>
 		<script type="text/javascript">
 			initialize();
